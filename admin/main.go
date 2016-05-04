11dd8c46018984b5c3e39696a28f1af40f602fa8
Allan MacDonald <amacdonald@thecape.ca>
2016-05-04 13:52:10 -0300

    added activity

--- admin/main.go
+++ admin/main.go
@@ -4,11 +4,12 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/gotstago/sandbox/admin/app/models"
+	"github.com/gotstago/sandbox/admin/config/routes"
 	"github.com/gotstago/sandbox/admin/model"
-    "github.com/gotstago/sandbox/admin/app/models"
-    "github.com/gotstago/sandbox/admin/config/routes"
 	"github.com/jinzhu/gorm"
 	_ "github.com/mattn/go-sqlite3"
+	"github.com/gotstago/activity"
 	"github.com/qor/admin"
 	"github.com/qor/qor"
 	// "github.com/qor/render"
@@ -19,15 +20,15 @@ var DataTypes = []string{"uint", "int", "string"}
 
 func main() {
 	DB, _ := gorm.Open("sqlite3", "demo.db")
-    //l10n.RegisterCallbacks(&DB)
+	//l10n.RegisterCallbacks(&DB)
 	DB.AutoMigrate(&models.User{}, &models.Product{}, &model.Entity{}, &model.EntityAttribute{}, &model.AttributeType{})
-
+	//AutoMigrate(&activity.QorActivity{})
 	// Register route
 	mux := http.NewServeMux()
-    
-    mux.Handle("/", routes.Router())
-	
-    // Initalize
+
+	mux.Handle("/", routes.Router())
+
+	// Initalize
 	Admin := admin.New(&qor.Config{DB: DB})
 
 	Admin.SetSiteName("Lighthouse Buy and Sell")
@@ -49,20 +50,22 @@ func main() {
 
 	// Create resources from GORM-backend model
 	//Design.AddResource(&Entity{})
-	//entity := 
-    Design.AddResource(&model.Entity{}, &admin.Config{Menu: []string{"Entity Management"}})
-    Design.AddResource(&model.AttributeType{}, &admin.Config{Menu: []string{"Lookups"}})
-    //entityAttributeMeta := entity.Meta(&admin.Meta{Name: "EntityAttributes"})
+	entity := Design.AddResource(&model.Entity{}, &admin.Config{Menu: []string{"Entity Management"}})
+	// Register Activity for Entity resource
+	activity.Register(entity)
+
+	Design.AddResource(&model.AttributeType{}, &admin.Config{Menu: []string{"Lookups"}})
+	//entityAttributeMeta := entity.Meta(&admin.Meta{Name: "EntityAttributes"})
 	//entityAttributeMeta.Resource.Meta(&admin.Meta{Name: "DataType", Type: "select_one", Collection: DataTypes})
-    //entity.Meta(&admin.Meta{Name: "DataType", Type: "select_one", Collection: DataTypes})
-    //Design.Meta(&admin.Meta{Name: "DataType", Type: "select_one", Collection: DataTypes})
+	//entity.Meta(&admin.Meta{Name: "DataType", Type: "select_one", Collection: DataTypes})
+	//Design.Meta(&admin.Meta{Name: "DataType", Type: "select_one", Collection: DataTypes})
 
 	// Add Dashboard
 	Design.AddMenu(&admin.Menu{Name: "Dashboard", Link: "/design"})
 
 	Design.MountTo("/design", mux)
-    
-    //config.View.Layout
+
+	//config.View.Layout
 
 	fmt.Println("Listening on: 9001")
 	//http.ListenAndServe(":9001", mux)
